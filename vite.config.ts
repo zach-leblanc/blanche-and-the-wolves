56f6b1b296f7dd3c250bb420d6757ff3c5af69fe
Zachary Leblanc <97243047+zach-leblanc@users.noreply.github.com>
2025-08-21 09:35:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Fixed for GitHub Pages project site: https://zach-leblanc.github.io/blanche-and-the-wolves/
 export default defineConfig({
   plugins: [react()],
-  base: '/blanche-and-the-wolves/',
+  base: '/',
 })
